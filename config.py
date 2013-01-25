04beeaa66e89d717d22ce54471408b006b0c70cb
Gregory Haynes <greg@greghaynes.net>
2013-01-24 16:08:06 -0800

    Runs statsd listener

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
+port = 8125
 percentThreshold = 90
 flushInterval = 10.0 # Seconds to flush stats
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from twisted.internet.protocol import DatagramProtocol
+from twisted.internet import reactor
+
 from collections import Counter, defaultdict
+import heapq
 
 import config
 
@@ -10,6 +13,13 @@ class StatsdListener(DatagramProtocol):
             'ms': self.handleTimer,
             'c': self.handleCounter,
         }
+        self.timers = defaultdict(list)
+        self.timers_sum = 0
+        self.timer_type_multipliers = {
+            'ms': 1,
+            's': 1000
+        }
+        self.counters = Counter()
 
     def datagramReceived(self, data, (host, port)):
         metrics = data.split('\n')
@@ -31,3 +41,23 @@ class StatsdListener(DatagramProtocol):
         self.type_handlers[event_type](event_name, event_type,
             event_val, sampling)
 
+    def handleTimer(self, event_name, event_type, event_val, sampling):
+        # Sampling is meaningless for a timer
+        if sampling != 1:
+            raise ValueError('Sampling specified for a timer type')
+        
+        event_val *= self.timer_type_multipliers[event_type]
+
+        heappush(self.timers[event_name], event_val)
+        self.timers_sum += event_val
+
+    def handleCounter(self, event_name, event_type, event_val, sampling):
+        self.counters[event_name] += event_val * sampling
+
+
+def main():
+    reactor.listenUDP(config.port, StatsdListener())
+    reactor.run()
+
+if __name__ == '__main__':
+    main()
