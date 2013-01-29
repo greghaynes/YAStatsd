1f11de4364de31aad58cc5d222017501a1b523b8
Gregory Haynes <greg@greghaynes.net>
2013-01-29 10:15:54 -0800

    Added setup.py, moved source into yastatsd dir, moved main.py to server.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='yastatsd',
+    version='0.1.0',
+    description='Yet Another Statsd Server',
+    author='Gregory Haynes',
+    author_email='greg@greghaynes.net',
+    url='http://github.com/greghaynes/YAStatsd',
+    license='MIT',
+    packages=['yastatsd'],
+    )
